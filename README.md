9340fef8bf558508f8092f070342f41ad508e975
Serhii Veremiichuk <veremey4uk@gmail.com>
2025-02-17 20:04:55 +0100

    Added cors

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 To see code changes you have to youse live-server
 
-```
+```JavaScript
 npx live-server client
 ```
 
 This command do not install npm packages or modules to your project.
+
+To run the application, don't forget to execute:
+
+```JavaScript
+node index.js
+```
+
+in the `server` directory."
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require("express")
+const cors = require("cors")
 const quotes = require("./data/quotes")
 const app = express()
 const PORT = 3000
 
 function getRandomQuote() {
 	const randomIndex = Math.floor(Math.random() * quotes.length)
-
 	return quotes[randomIndex]
 }
 
+app.use(cors())
 app.get("/quotes/random-single", (req, res) => {
 	const randomQuote = getRandomQuote()
 	res.json(randomQuote)
